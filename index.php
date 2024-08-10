3f02763dc0aebaf912284177ab2e890d01729343
endigo6776 <endigo6776@protonmail.com>
2024-08-10 14:21:02 +0300

    improve user experience

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+// Syntax dasar PHP
+// echo "Halo semua";
+
+
+// Penggunana Variabel pada PHP
+// $nama = "Novan rohman";
+// $kelas = "XII-RPL";
+
+// echo $nama;
+// echo "</br>";
+// echo $kelas;
+// echo "</br>";
+// echo "hai, perkenalkan nama saya ".$nama." saya dari kelas " .$kelas;
+
+// tipe data pada php
+// String, integer, float, boolean, array, object, null
+
+// contoh penggunaan array
+// $anggota = array("Andi","Budi","Joni");
+
+// strlen() untuk menghitung jumlah karakter
+// $kalimat = "Nama saya adalah";
+// echo strlen($kalimat);
+
+// str_word_count() untuk menghitung jumlah kalimat
+// $kalimat = "Nama saya adalah";
+// echo str_word_count($kalimat);
+
+// Membalik kalimat dengan strrev()
+// $kalimat = "Nama saya adalah";
+// echo strrev($kalimat);
+
+// replace kalimat dengan str_replace()
+// $kalimat = "Nama saya adalah";
+// echo str_replace("Nama","Hai",$kalimat);
+?>
